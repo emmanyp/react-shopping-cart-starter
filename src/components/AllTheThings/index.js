3168baacd3015b83a3e3cd26b7ae7371e3ed4f5c
emma <emma.ronaldo7@gmail.com>
2021-12-06 15:31:53 -0500

    addToCart

--- src/components/AllTheThings/index.js
+++ src/components/AllTheThings/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
-function AllTheThings(props) {
+const AllTheThings = (props) => {
+  const things = props.productsArr.map((elem, index) => {
+		return( 
  
+        <li onClick= {()=>{props.addToCart(index)}}>
+          {elem.name} - ${elem.price}
+          </li>
+)
+	});
   return (
-    <div className="AllTheThings">
-       <h2>Put these in your cart!</h2>
-
-    </div>
-      
-
-  );
+		<div className='AllTheThings'>
+			<h2>Put these in your cart!</h2>
+			<ul> {things}</ul>
+			<br />
+		</div>
+	);
 }
 
 export default AllTheThings;
--- src/components/MyShoppingCart/index.js
+++ src/components/MyShoppingCart/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 function MyShoppingCart(props) {
-
+const things = props.cart.map((elem, index) => {
+	return (
+		<li
+			onClick={() => {
+				props.addToCart(index);
+			}}>
+			{elem.name} - ${elem.price}
+		</li>
+	);
+});
   return (
     <div className="MyShoppingCart">
       <h2>Your Cart!</h2>
-
+      <ul>{things}</ul>
     </div>
   );
 }
